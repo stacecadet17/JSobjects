b10485d1c9c5dc257d676e716b80d47ad2f1c965
stacecadet17 <staceyabbott@stacey-abbotts-macbook.local>
2017-10-03 18:45:11 -0700

    second commit

--- jsObjects.js
+++ jsObjects.js
@@ -17,3 +17,15 @@ for (var key in users){
       console.log(parseInt(item) + 1 + " - "+users[key][item].last_name.toUpperCase()+", "+users[key][item].first_name.toUpperCase()+" - "+(users[key][item].last_name.length+users[key][item].first_name.length))
   }
 }
+
+//easier and simpler way...
+for (const key in users) {
+           console.log(key.toUpperCase());
+           for (let i = 0; i < users[key].length; i++) {
+               const num = i + 1;
+               const fname = users[key][i].first_name;
+               const lname = users[key][i].last_name;
+               const length = fname.length + lname.length;
+               console.log(`${num} - ${lname}, ${fname} - ${length}`);
+           }
+       };
